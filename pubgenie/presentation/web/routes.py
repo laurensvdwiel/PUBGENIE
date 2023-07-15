91a971583ca027b5b287ac9a47a2a42993cadc62
laurensvdwiel <laurensvdwiel@gmail.com>
2023-07-14 18:16:20 -0700

    Added bulma framework; added routing for two methods

--- pubgenie/presentation/web/routes.py
+++ pubgenie/presentation/web/routes.py
@@ -1,26 +1,26 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from pubgenie.controller.job import execute_query
 
-bp = Blueprint('web', __name__)#, template_folder='templates', static_folder='static')
+bp = Blueprint('web', __name__)
 
 @bp.route('/')
 def index():
     return redirect(url_for('web.home'))
 
-@bp.route('/pubgenie/gene/<gene>', methods=['GET', 'POST'])
-def gene(gene):
-    result = execute_query(gene)
-    return render_template('index.html', result=result)
-
 @bp.route('/pubgenie/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         user_input = request.form.get('user_input')
-        result = execute_query(user_input)
-        return render_template('index.html', result=result)
+        query_type = request.form.get('query_type')
+        return redirect(url_for('web.' + query_type, gene=user_input))
     return render_template('index.html')
+
+@bp.route('/pubgenie/gene/<gene>', methods=['GET', 'POST'])
+def gene(gene):
+    result = execute_query(gene)
+    return render_template('index.html', result=result, query_type='gene', user_input=gene)
 
 @bp.route('/pubgenie/pos/<pos>', methods=['GET', 'POST'])
 def pos(pos):
-    # Your code here
-    pass
+    result = execute_query(pos)
+    return render_template('index.html', result=result, query_type='pos', user_input=pos)
